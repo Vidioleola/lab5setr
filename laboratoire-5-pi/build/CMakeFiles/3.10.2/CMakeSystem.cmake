bd3b880de1759bc6f0ba6bd45b098008f1e1b1bd
edpoptart <edward.poulin-pepin.1@ulaval.ca>
2020-04-30 16:52:07 -0400

    configured project for WSL

--- laboratoire-5-pi/build/CMakeFiles/3.10.2/CMakeSystem.cmake
+++ laboratoire-5-pi/build/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_HOST_SYSTEM "Linux-4.19.114-1-MANJARO")
+set(CMAKE_HOST_SYSTEM "Linux-4.4.0-17763-Microsoft")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "4.19.114-1-MANJARO")
+set(CMAKE_HOST_SYSTEM_VERSION "4.4.0-17763-Microsoft")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/home/vincent/arm-cross-comp-env/rpi-zero-w-toolchain.cmake")
+include("/home/epepi/arm-cross-comp-env/rpi-zero-w-toolchain.cmake")
 
 set(CMAKE_SYSTEM "Linux-4.19")
 set(CMAKE_SYSTEM_NAME "Linux")
